81554c48547199dee47183552f78637e91a3c950
PolyakovDmitry546 <65275577+PolyakovDmitry546@users.noreply.github.com>
2023-10-05 11:44:13 +0500

    Add django-silk

--- django_project/config/settings.py
+++ django_project/config/settings.py
@@ -39,9 +39,11 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
 
+    'silk',
+
     'mptt',
     'ckeditor',
-    
+
     'project',
     'contact',
 ]
@@ -54,6 +56,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'silk.middleware.SilkyMiddleware',
 ]
 
 ROOT_URLCONF = 'config.urls'
