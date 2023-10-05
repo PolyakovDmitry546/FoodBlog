609a7417c6cdd86a5136861531666adcb7dffd08
PolyakovDmitry546 <65275577+PolyakovDmitry546@users.noreply.github.com>
2023-10-05 11:28:19 +0500

    Add sidebar to home page

--- django_project/project/templatetags/menu_tags.py
+++ django_project/project/templatetags/menu_tags.py
@@ -4,9 +4,20 @@ from project.models import Category, Post
 
 register = template.Library()
 
+
+def get_all_categories():
+    return Category.objects.all()
+
+
+@register.simple_tag()
+def categories():
+    category = get_all_categories()
+    return category
+
+
 @register.inclusion_tag('project/include/tags/top_menu.html')
 def get_categories():
-    category = Category.objects.all()
+    category = get_all_categories()
     return {'list_category': category}
 
 
