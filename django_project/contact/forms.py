9a6e1ac209f7e63d84100697d9ab549948b96a41
PolyakovDmitry546 <65275577+PolyakovDmitry546@users.noreply.github.com>
2023-10-30 12:09:28 +0500

    Fix contact form

--- django_project/contact/forms.py
+++ django_project/contact/forms.py
@@ -2,14 +2,8 @@ from django import forms
 
 from .models import ContactModel
 
+
 class ContactForm(forms.ModelForm):
     class Meta:
         model = ContactModel
         fields = '__all__'
-
-"""         widgets = {
-            'name': forms.TextInput(),
-            'email': forms.TextInput(),
-            'website': forms.TextInput(),
-            'message': forms.TextInput()
-        } """
